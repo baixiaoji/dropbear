84401a4f3c0485a3cd19a203e3246bc78fc1fd79
baixiaoji <1041874421@qq.com>
2020-05-03 14:27:43 +0800

    finish the evalate.js

--- src/evaluate.js
+++ src/evaluate.js
@@ -1,6 +1,32 @@
 const { environment } = require('./standard-library');
 const last = collection => collection[collection.length - 1];
 
-const evaluate = () => {};
+const apply = (node) => {
+  const fn = environment[node.name];
+
+  const args = node.arguments.map(evaluate);
+
+  if (typeof fn !== 'function') {
+    throw TypeError(`${node.name} is not a function`);
+  }
+
+  return fn.apply(null, args);
+}
+
+const getIndentifier = (node) => {
+  const identifier = environment[node.name];
+
+  if (typeof identifier === 'undefined') {
+    throw new ReferenceError(`${node.name} is not defined`);
+  }
+
+  return identifier;
+}
+
+const evaluate = (node) => {
+  if (node.type === 'CallExpression') return apply(node);
+  if (node.type === 'Identifier') return getIndentifier(node);
+  if (node.value) return node.value;
+};
 
 module.exports = { evaluate };
